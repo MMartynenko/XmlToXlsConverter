68e3f8fe7fdc26458be09702a7a1b804ad871f6e
Mark Martynenko <Mark_Martynenko@epam.com>
2022-02-08 18:18:47 +0300

    Add project files.

---
+++ XmlToXlsConverter/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace XmlToXlsConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtXmlFilePath = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chkCustomName = new System.Windows.Forms.CheckBox();
+            this.txtCustomFileName = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.OFD = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select XML file to convert";
+            // 
+            // txtXmlFilePath
+            // 
+            this.txtXmlFilePath.Location = new System.Drawing.Point(13, 42);
+            this.txtXmlFilePath.Name = "txtXmlFilePath";
+            this.txtXmlFilePath.Size = new System.Drawing.Size(323, 31);
+            this.txtXmlFilePath.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(350, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chkCustomName
+            // 
+            this.chkCustomName.AutoSize = true;
+            this.chkCustomName.Location = new System.Drawing.Point(13, 95);
+            this.chkCustomName.Name = "chkCustomName";
+            this.chkCustomName.Size = new System.Drawing.Size(159, 29);
+            this.chkCustomName.TabIndex = 3;
+            this.chkCustomName.Text = "Excel File Name";
+            this.chkCustomName.UseVisualStyleBackColor = true;
+            // 
+            // txtCustomFileName
+            // 
+            this.txtCustomFileName.Location = new System.Drawing.Point(179, 95);
+            this.txtCustomFileName.Name = "txtCustomFileName";
+            this.txtCustomFileName.Size = new System.Drawing.Size(283, 31);
+            this.txtCustomFileName.TabIndex = 4;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 148);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(323, 34);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(350, 147);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 34);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Convert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // OFD
+            // 
+            this.OFD.Filter = "XML File (*.xml)|*.xml|All files (*.*)|*.*";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 197);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.txtCustomFileName);
+            this.Controls.Add(this.chkCustomName);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtXmlFilePath);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Xml to Xls Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtXmlFilePath;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox chkCustomName;
+        private System.Windows.Forms.TextBox txtCustomFileName;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog OFD;
+    }
+}
+
---
+++ XmlToXlsConverter/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Runtime.InteropServices;
+using Microsoft.Office.Interop.Excel;
+using System.IO;
+
+namespace XmlToXlsConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult drResult = OFD.ShowDialog();
+            if (drResult == System.Windows.Forms.DialogResult.OK)
+                txtXmlFilePath.Text = OFD.FileName;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+            if (chkCustomName.Checked && txtCustomFileName.Text != "" && txtXmlFilePath.Text != "") // using Custome Xml File Name  
+            {
+                if (File.Exists(txtXmlFilePath.Text))
+                {
+                    string CustXmlFilePath = Path.Combine(new FileInfo(txtXmlFilePath.Text).DirectoryName, txtCustomFileName.Text); // Ceating Path for Xml Files  
+                    System.Data.DataTable dt = CreateDataTableFromXml(txtXmlFilePath.Text);
+                    ExportDataTableToExcel(dt, CustXmlFilePath);
+
+                    MessageBox.Show("Conversion completed");
+                }
+
+            }
+            else if (!chkCustomName.Checked || txtXmlFilePath.Text != "") // Using Default Xml File Name  
+            {
+                if (File.Exists(txtXmlFilePath.Text))
+                {
+                    FileInfo fi = new FileInfo(txtXmlFilePath.Text);
+                    string XlFile = fi.DirectoryName + "\\" + fi.Name.Replace(fi.Extension, ".xlsx");
+                    System.Data.DataTable dt = CreateDataTableFromXml(txtXmlFilePath.Text);
+                    ExportDataTableToExcel(dt, XlFile);
+
+                    MessageBox.Show("Conversion completed");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please fill required fields");
+            }
+        }
+
+        public System.Data.DataTable CreateDataTableFromXml(string XmlFile)
+        {
+
+            System.Data.DataTable Dt = new System.Data.DataTable();
+            try
+            {
+                DataSet ds = new DataSet();
+                ds.ReadXml(XmlFile);
+                Dt.Load(ds.CreateDataReader());
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return Dt;
+        }
+
+        private void ExportDataTableToExcel(System.Data.DataTable table, string Xlfile)
+        {
+
+            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+            Workbook book = excel.Application.Workbooks.Add(Type.Missing);
+            excel.Visible = false;
+            excel.DisplayAlerts = false;
+            Worksheet excelWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)book.ActiveSheet;
+            excelWorkSheet.Name = table.TableName;
+
+            progressBar1.Maximum = table.Columns.Count;
+            for (int i = 1; i < table.Columns.Count + 1; i++) // Creating Header Column In Excel  
+            {
+                excelWorkSheet.Cells[1, i] = table.Columns[i - 1].ColumnName;
+                if (progressBar1.Value < progressBar1.Maximum)
+                {
+                    progressBar1.Value++;
+                    int percent = (int)(((double)progressBar1.Value / (double)progressBar1.Maximum) * 100);
+                    progressBar1.CreateGraphics().DrawString(percent.ToString() + "%", new System.Drawing.Font("Arial", (float)8.25, FontStyle.Regular), Brushes.Black, new PointF(progressBar1.Width / 2 - 10, progressBar1.Height / 2 - 7));
+                    System.Windows.Forms.Application.DoEvents();
+                }
+            }
+
+
+            progressBar1.Maximum = table.Rows.Count;
+            for (int j = 0; j < table.Rows.Count; j++) // Exporting Rows in Excel  
+            {
+                for (int k = 0; k < table.Columns.Count; k++)
+                {
+                    excelWorkSheet.Cells[j + 2, k + 1] = table.Rows[j].ItemArray[k].ToString();
+                }
+
+                if (progressBar1.Value < progressBar1.Maximum)
+                {
+                    progressBar1.Value++;
+                    int percent = (int)(((double)progressBar1.Value / (double)progressBar1.Maximum) * 100);
+                    progressBar1.CreateGraphics().DrawString(percent.ToString() + "%", new System.Drawing.Font("Arial", (float)8.25, FontStyle.Regular), Brushes.Black, new PointF(progressBar1.Width / 2 - 10, progressBar1.Height / 2 - 7));
+                    System.Windows.Forms.Application.DoEvents();
+                }
+            }
+
+
+            book.SaveAs(Xlfile);
+            book.Close(true);
+            excel.Quit();
+
+            Marshal.ReleaseComObject(book);
+            Marshal.ReleaseComObject(book);
+            Marshal.ReleaseComObject(excel);
+
+        }
+    }
+}
